019ca691f1f7fd6d740d2c6449c2782e4afd2191
Miguel Santiago <miguelsantiago1812@gmail.com>
2021-10-04 20:47:58 -0300

    Aperfeiçoando o visual do código

--- src/funcao/ConversaoUtil.java
+++ src/funcao/ConversaoUtil.java
@@ -6,27 +6,27 @@ import java.util.List;
 
 public class ConversaoUtil {
 	public static void converterDecimalParaBinarios(int decimal) {
-	    
-	    List<Integer> list = new ArrayList<Integer>();
-	    while(decimal > 0) {
-	        list.add(decimal % 2);
-	        decimal = decimal / 2;
-	    }
-	    Collections.reverse(list);
-	    for (Integer integer : list) {
-	        System.out.print(integer);
-	    }
+
+		List<Integer> list = new ArrayList<Integer>();
+		while (decimal > 0) {
+			list.add(decimal % 2);
+			decimal = decimal / 2;
+		}
+		Collections.reverse(list);
+		for (Integer binario : list) {
+			System.out.print(binario);
+		}
 	}
-	
+
 	public static void converterBinarioParaDecimal(String binario) {
-	    int soma = 0;
-	    int contador  = 0;
-	    for(int i = binario.length() - 1; i >= 0; i--) {
-	    soma = (int) (soma + Math.pow(2, contador) * Character.getNumericValue(binario.charAt(i)));
-	    contador ++;
-	    }
-	    
-	System.out.println(soma);
+		int soma = 0;
+		int contador = 0;
+		for (int i = binario.length() - 1; i >= 0; i--) {
+			soma = (int) (soma + Math.pow(2, contador) * Character.getNumericValue(binario.charAt(i)));
+			contador++;
+		}
+
+		System.out.println(soma);
 	}
-	 
+
 }
