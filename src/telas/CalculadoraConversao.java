b00e5e366e1c76a680998efedc9e3f5ba19a24e3
Alice Machado Koch <machado.alice.koch@gmail.com>
2021-10-18 22:05:26 -0300

    Design

--- src/telas/CalculadoraConversao.java
+++ src/telas/CalculadoraConversao.java
@@ -60,7 +60,7 @@ public class CalculadoraConversao extends JFrame {
 	public CalculadoraConversao() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(CalculadoraConversao.class.getResource("/imagens/akatsuki.png")));
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setBounds(100, 100, 869, 634);
+		setBounds(100, 100, 715, 634);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(38, 38, 38));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -69,7 +69,7 @@ public class CalculadoraConversao extends JFrame {
 
 		var panel1 = new JPanel();
 		panel1.setBackground(new Color(26, 26, 26));
-		panel1.setBounds(0, 130, 843, 465);
+		panel1.setBounds(0, 130, 710, 465);
 		contentPane.add(panel1);
 		panel1.setLayout(null);
 
@@ -97,7 +97,7 @@ public class CalculadoraConversao extends JFrame {
 		btnMultiplicacao.setForeground(Color.WHITE);
 		btnMultiplicacao.setBackground(Color.DARK_GRAY);
 		btnMultiplicacao.setFont(new Font("Dialog", Font.PLAIN, 16));
-		btnMultiplicacao.setBounds(622, 182, 73, 53);
+		btnMultiplicacao.setBounds(497, 183, 73, 53);
 		panel1.add(btnMultiplicacao);
 
 		var btnAdicao = new JButton("+");
@@ -114,7 +114,7 @@ public class CalculadoraConversao extends JFrame {
 		btnAdicao.setForeground(Color.WHITE);
 		btnAdicao.setBackground(Color.DARK_GRAY);
 		btnAdicao.setFont(new Font("Dialog", Font.PLAIN, 20));
-		btnAdicao.setBounds(622, 127, 73, 53);
+		btnAdicao.setBounds(497, 128, 73, 53);
 		panel1.add(btnAdicao);
 
 		var btnSubtracao = new JButton("-");
@@ -131,7 +131,7 @@ public class CalculadoraConversao extends JFrame {
 		btnSubtracao.setForeground(Color.WHITE);
 		btnSubtracao.setBackground(Color.DARK_GRAY);
 		btnSubtracao.setFont(new Font("Dialog", Font.PLAIN, 20));
-		btnSubtracao.setBounds(622, 72, 73, 53);
+		btnSubtracao.setBounds(497, 73, 73, 53);
 		panel1.add(btnSubtracao);
 
 		var btnCalcularResultado = new JButton("=");
@@ -149,7 +149,7 @@ public class CalculadoraConversao extends JFrame {
 		btnCalcularResultado.setForeground(Color.WHITE);
 		btnCalcularResultado.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnCalcularResultado.setBackground(SystemColor.activeCaption);
-		btnCalcularResultado.setBounds(622, 238, 73, 107);
+		btnCalcularResultado.setBounds(497, 239, 73, 107);
 		panel1.add(btnCalcularResultado);
 
 		var btnNum3 = new JButton("3");
@@ -165,10 +165,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum3.setForeground(Color.WHITE);
 		btnNum3.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum3.setBackground(SystemColor.desktop);
-		btnNum3.setBounds(455, 72, 157, 87);
+		btnNum3.setBounds(330, 73, 157, 87);
 		panel1.add(btnNum3);
 
 		var btnNum5 = new JButton("5");
+		btnNum5.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNum5.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/kakuzu5.png")));
 		btnNum5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "5");
@@ -178,10 +180,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum5.setForeground(Color.WHITE);
 		btnNum5.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum5.setBackground(SystemColor.desktop);
-		btnNum5.setBounds(358, 165, 122, 87);
+		btnNum5.setBounds(170, 166, 150, 87);
 		panel1.add(btnNum5);
 
 		var btnNum7 = new JButton("7");
+		btnNum7.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNum7.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/sasori7.png")));
 		btnNum7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "7");
@@ -191,10 +195,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum7.setForeground(Color.WHITE);
 		btnNum7.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum7.setBackground(SystemColor.desktop);
-		btnNum7.setBounds(228, 258, 120, 87);
+		btnNum7.setBounds(10, 259, 150, 87);
 		panel1.add(btnNum7);
 
 		var btnNum9 = new JButton("9");
+		btnNum9.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNum9.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/itachi9.png")));
 		btnNum9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "9");
@@ -204,10 +210,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum9.setForeground(Color.WHITE);
 		btnNum9.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum9.setBackground(SystemColor.desktop);
-		btnNum9.setBounds(490, 258, 122, 87);
+		btnNum9.setBounds(330, 259, 157, 87);
 		panel1.add(btnNum9);
 
 		var btnNum6 = new JButton("6");
+		btnNum6.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/pain6.png")));
+		btnNum6.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNum6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "6");
@@ -217,10 +225,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum6.setForeground(Color.WHITE);
 		btnNum6.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum6.setBackground(SystemColor.desktop);
-		btnNum6.setBounds(490, 165, 122, 87);
+		btnNum6.setBounds(330, 166, 157, 87);
 		panel1.add(btnNum6);
 
 		var btnNum8 = new JButton("8");
+		btnNum8.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/kisame8.png")));
+		btnNum8.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNum8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "8");
@@ -230,10 +240,12 @@ public class CalculadoraConversao extends JFrame {
 		btnNum8.setForeground(Color.WHITE);
 		btnNum8.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum8.setBackground(SystemColor.desktop);
-		btnNum8.setBounds(358, 258, 122, 87);
+		btnNum8.setBounds(170, 259, 150, 87);
 		panel1.add(btnNum8);
 
 		var btnNum4 = new JButton("4");
+		btnNum4.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNum4.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/deidara4.png")));
 		btnNum4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "4");
@@ -243,7 +255,7 @@ public class CalculadoraConversao extends JFrame {
 		btnNum4.setForeground(Color.WHITE);
 		btnNum4.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum4.setBackground(SystemColor.desktop);
-		btnNum4.setBounds(228, 165, 120, 87);
+		btnNum4.setBounds(10, 166, 150, 87);
 		panel1.add(btnNum4);
 
 		var btnNum1 = new JButton("1");
@@ -264,7 +276,7 @@ public class CalculadoraConversao extends JFrame {
 		btnNum1.setForeground(Color.WHITE);
 		btnNum1.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum1.setBackground(SystemColor.desktop);
-		btnNum1.setBounds(113, 72, 122, 87);
+		btnNum1.setBounds(10, 73, 150, 87);
 		panel1.add(btnNum1);
 
 		var btnNum2 = new JButton("2");
@@ -279,19 +291,19 @@ public class CalculadoraConversao extends JFrame {
 		btnNum2.setForeground(Color.WHITE);
 		btnNum2.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum2.setBackground(SystemColor.desktop);
-		btnNum2.setBounds(286, 67, 150, 87);
+		btnNum2.setBounds(170, 73, 150, 87);
 		panel1.add(btnNum2);
 
 		var rdbEntradaBinario = new JRadioButton("Bin\u00E1ria");
-		rdbEntradaBinario.setBounds(713, 146, 109, 23);
+		rdbEntradaBinario.setBounds(576, 147, 109, 23);
 		panel1.add(rdbEntradaBinario);
 		
 		var rdbEntradaDecimal = new JRadioButton("Decimal");
-		rdbEntradaDecimal.setBounds(713, 72, 109, 23);
+		rdbEntradaDecimal.setBounds(576, 73, 109, 23);
 		panel1.add(rdbEntradaDecimal);
 		
 		var rdbEntradaHexadecimal = new JRadioButton("Hexadecimal");
-		rdbEntradaHexadecimal.setBounds(713, 107, 109, 23);
+		rdbEntradaHexadecimal.setBounds(576, 108, 109, 23);
 		panel1.add(rdbEntradaHexadecimal);
 		
 		rdbEntradaDecimal.addActionListener(new ActionListener() {
@@ -319,6 +331,8 @@ public class CalculadoraConversao extends JFrame {
 		});
 
 		var btnNum0 = new JButton("0");
+		btnNum0.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNum0.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/hidan0.png")));
 		btnNum0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tpDigitacao.setText(tpDigitacao.getText() + "0");
@@ -328,7 +342,7 @@ public class CalculadoraConversao extends JFrame {
 		btnNum0.setForeground(Color.WHITE);
 		btnNum0.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnNum0.setBackground(Color.BLACK);
-		btnNum0.setBounds(358, 352, 122, 87);
+		btnNum0.setBounds(170, 353, 150, 87);
 		panel1.add(btnNum0);
 
 		var btnLimpar = new JButton("Limpar");
@@ -340,26 +354,26 @@ public class CalculadoraConversao extends JFrame {
 		btnLimpar.setForeground(Color.WHITE);
 		btnLimpar.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnLimpar.setBackground(Color.BLACK);
-		btnLimpar.setBounds(50, 11, 383, 45);
+		btnLimpar.setBounds(10, 11, 560, 45);
 		panel1.add(btnLimpar);
 		
 		var lblNarutinho = new JLabel("");
 		lblNarutinho.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNarutinho.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblNarutinho.setIcon(new ImageIcon(CalculadoraConversao.class.getResource("/imagens/narutinho.png")));
-		lblNarutinho.setBounds(738, 182, 57, 53);
+		lblNarutinho.setBounds(601, 183, 57, 53);
 		panel1.add(lblNarutinho);
 		
 		var rdbSaidaDecimal = new JRadioButton("Decimal");
-		rdbSaidaDecimal.setBounds(713, 248, 109, 23);
+		rdbSaidaDecimal.setBounds(576, 249, 109, 23);
 		panel1.add(rdbSaidaDecimal);
 		
 		var rdbSaidaHexadecimal = new JRadioButton("Hexadecimal");
-		rdbSaidaHexadecimal.setBounds(713, 283, 109, 23);
+		rdbSaidaHexadecimal.setBounds(576, 284, 109, 23);
 		panel1.add(rdbSaidaHexadecimal);
 		
 		var rdbSaidaBinario = new JRadioButton("Bin\u00E1ria");
-		rdbSaidaBinario.setBounds(713, 322, 109, 23);
+		rdbSaidaBinario.setBounds(576, 323, 109, 23);
 		panel1.add(rdbSaidaBinario);
 		
 		rdbSaidaDecimal.addActionListener(new ActionListener() {
